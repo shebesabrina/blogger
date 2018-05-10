cf1a0d7af38bd0c4792fc6a2ec7ffbfa565a510b
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-10 16:07:43 -0600

    Added feature test for show page.

--- spec/features/user_sees_single_article.rb
+++ spec/features/user_sees_single_article.rb
@@ -3,13 +3,25 @@ require "rails_helper"
 describe "user sees one articles" do
   describe "they visit /articles/1" do
     it "displays all articles" do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
-      article_2 = Article.create!(title: "Title 2", body: "Body 2")
+      article_1 = Article.create(title: "Title 1", body: "Body 1")
+      article_2 = Article.create(title: "Title 2", body: "Body 2")
 
       visit '/articles/1'
 
       expect(page).to have_link(article_1.title)
       expect(page).to not_have_link(article_2.title)
+    end
+    describe "they link from the article index" do
+      it "displays information for one article" do
+        article = Article.create(title: "New Title", body: "New Body")
+
+        visit articles_path
+
+        click_link article.title
+
+        expect(page).to have_content(article.title)
+        expect(page).to have_content(article.body)
+      end
     end
   end
 end
