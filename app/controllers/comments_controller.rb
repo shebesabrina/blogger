542c3dde0c20ac29df4208f68d7f0f7d15ab9bab
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-17 10:34:15 -0600

    comments controller refactored

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,15 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
-    @comment.article_id = params[:article_id]
+    article = Article.find(params[:article_id])
+    @comment = article.comments.create(comment_params)
+    # @comment = Comment.new(comment_params)
+    # @comment.article_id = params[:article_id]
 
-    @comment.save
+    redirect_to article_path(article)
+  end
 
-    redirect_to article_path(@comment.article)
+  def new
+    @comment = Comment.new
   end
 
   private
